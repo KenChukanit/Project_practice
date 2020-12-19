f05580e98a405ce7c87046d675c1da3ff65b6c3d
KenChukanit <chonlasek.c@gmail.com>
2020-12-18 23:05:48 -0800

    add show page and all index page

--- Super_Team_Picker/routes/cohorts.js
+++ Super_Team_Picker/routes/cohorts.js
@@ -9,19 +9,45 @@ router.get('/new',(request,response)=>{
     response.render('cohorts/new', {cohort: false});
 })
 
-
-
-
 //Index page which mean /cohorts
 router.get('/',(request,response)=>{
     knex('cohorts')
         .orderBy('created_at','desc')
-        .then(lists => {
-            response.render(('cohorts/index'),{lists});
+        .then(cohorts => {
+            response.render(('cohorts/index'),{cohorts});
         })
 });
 
+//Add detail of cohort when adding a new cohort
+router.get('/:id',(request,response)=>{
+    knex('cohorts')
+        .where('id', request.params.id)
+        .first()
+        .then((cohort)=>{
+            if(!cohort){
+                response.send('No list found');
+            }else{
+                response.render('cohorts/show', {cohort})
+            }
+        })
+})
 
+//create a new cohort
+router.post('/',(request,response)=>{
+    const logo_url= request.body.logo_url
+    const cohort_name= request.body.cohort_name
+    const members= request.body.members
+    knex('cohorts')
+        .insert({
+            logo_url,
+            cohort_name,
+            members,
+        },'*')
+        .then(cohorts=>{
+            const cohort = cohorts[0];
+            response.redirect(`/cohorts/${cohort.id}`)
+        });
+});
 
 
 
